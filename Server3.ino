62a0cc32335c668a9054d1b329aedb620a8cdde0
ManuelMorenoT <62701278+ManuelMorenoT@users.noreply.github.com>
2022-03-16 08:07:10 -0600

    Add files via upload

---
+++ Server3.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+#ifndef STASSID
+#define STASSID "INFINITUMAC67_2.4"
+#define STAPSK  "r8jsZXbyZJ"
+#endif
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+WiFiServer server(80); 
+WiFiClient client;
+boolean alreadyConnected = false;
+int val;
+int Periodo = 10000;
+unsigned long Tiempo;
+String Data;
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop(void) {
+  client = server.available();
+  if(client){
+    Serial.println("Client connected");
+    while(client.connected()){
+       for(int i=0; i<5; i++){
+         val = analogRead(A0);
+         Data+=(String(val) + " ");
+         }
+         Serial.println(Data);
+         client.println(Data);
+         delay(33);//Cambiar por millitime
+         Data="";
+      }
+    }
+  
+  
+}
